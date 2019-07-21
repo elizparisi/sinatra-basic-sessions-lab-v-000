da0af2cda5ad219d0936daf1bc2220ef18b6dd9f
Liz P <parisielizabeth@gmail.com>
2019-07-21 02:00:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   
   post '/checkout' do
     session[:item] = params[:item]
-    @session = sessionb :checkout
+    @session = session :checkout
     
     erb: checkout
   end
